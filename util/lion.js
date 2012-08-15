e871371b14def34532e2d361a9e086337e7a0d65
supersheep <supersheepxjy@gmail.com>
2012-08-15 14:15:34 +0800

    css parser in main

--- util/lion.js
+++ util/lion.js
@@ -1,6 +1,14 @@
 function get(key,callback){
 	var data = '[["http://i1.s1.static.dp/trunk","http://i2.s1.static.dp/trunk","http://i3.s1.static.dp/trunk"],["http://i1.s2.static.dp/trunk","http://i2.s2.static.dp/trunk","http://i3.s2.static.dp/trunk"],["http://i1.s3.static.dp/trunk","http://i2.s3.static.dp/trunk","http://i3.s3.static.dp/trunk"],["http://i1.s4.static.dp/trunk","http://i2.s4.static.dp/trunk","http://i3.s4.static.dp/trunk"]]';
-	callback(null,data);
+	var json;
+
+	try{
+		json = JSON.parse(data);
+	}catch(e){
+		callback(e);
+	}
+
+	callback(null,json);
 }
 
-module.exports = get;
+exports.get = get;
