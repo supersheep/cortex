3ef92227b38bc2a0f0cec345c492f2dfdb69f11b
supersheep <supersheepxjy@gmail.com>
2012-09-17 17:53:29 +0800

    get dbconfig from lion

--- util/lion.js
+++ util/lion.js
@@ -1,14 +1,14 @@
+var pairs = {
+	"dp-common-web.imgResourceServer":'[["i1.s1.static.dp","i2.s1.static.dp","i3.s1.static.dp"],["i1.s2.static.dp","i2.s2.static.dp","i3.s2.static.dp"],["i1.s3.static.dp","i2.s3.static.dp","i3.s3.static.dp"],["i1.s4.static.dp","i2.s4.static.dp","i3.s4.static.dp"]]',
+	"dbhost":"192.168.8.44",
+	"dp-common-service.common.master.jdbc.username":"dpcom_comm",
+	"dp-common-service.common.master.jdbc.password":"dp!@C5NsYGTPg",
+	"dp-common-service.common.master.jdbc.url":"jdbc:mysql://192.168.8.44:3306/DianPingCOMM?characterEncoding=UTF8"
+}
+
 function get(key,callback){
-	var data = '[["i1.s1.static.dp","i2.s1.static.dp","i3.s1.static.dp"],["i1.s2.static.dp","i2.s2.static.dp","i3.s2.static.dp"],["i1.s3.static.dp","i2.s3.static.dp","i3.s3.static.dp"],["i1.s4.static.dp","i2.s4.static.dp","i3.s4.static.dp"]]';
-	var json;
 
-	try{
-		json = JSON.parse(data);
-	}catch(e){
-		callback(e);
-	}
-
-	callback(null,json);
+	callback(null,pairs[key]);
 }
 
 exports.get = get;
