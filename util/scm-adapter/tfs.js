0f21db6d1cc652870138be257a2b849a7462dae4
Kael <i@kael.me>
2012-10-30 12:23:26 +0800

    update all

--- util/scm-adapter/tfs.js
+++ util/scm-adapter/tfs.js
@@ -1,21 +1,36 @@
 var
 
 spawn = require('./spawn'),
+path = require('path'),
+fs_more = require('../fs-more'),
 tracer = require('tracer').colorConsole(),
-EventProxy = require('../event-proxy');
+EventProxy = require('../event-proxy'),
+
+REGEX_IS_SLN_FILE = /\.sln$/i;
 
 
-function SVN(params){
+function TFS(params){
     this.cwd = params.cwd;
     this.branch = params.branch;
     this.remote = params.remote;
 };
 
 
-SVN.prototype = {
+TFS.is = function(cwd){
+    var
+    dir_content = fs.readdirSync(cwd);
+    
+    return dir_content.some(function(current){
+        return REGEX_IS_SLN_FILE.test(current);
+    });
+};
+
+
+TFS.prototype = {
     pull: function(callback){
         spawn('svn', ['update'], {
             cwd: this.cwd
+            
         }, function(){
             callback();
         });
