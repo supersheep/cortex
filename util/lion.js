672e684626ccf23121b761870f116d634352ccff
supersheep <supersheepxjy@gmail.com>
2012-11-21 14:17:46 +0800

    fix lion

--- util/lion.js
+++ util/lion.js
@@ -8,10 +8,10 @@ function get(opt,callback){
 
 	if(["alpha","qa","pro"].indexOf(env) == -1){
 		callback(new Error("请传入环境参数，可为alpha,qa或pro"));
-	}else if(pattern){
+	}else if(!pattern){
 		callback(new Error("请传入参数指定lionaddr"));
 	}else{	
-		console.log("请求"+url+"获取lion配置");
+		console.log("请求获取lion配置");
 		request(url, function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 		  	callback(null,body);
