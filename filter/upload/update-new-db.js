ef643d05f58ff2e4083a209bf54daab4664301c0
supersheep <supersheepxjy@gmail.com>
2012-11-21 13:28:17 +0800

    修改db为构造器，添加更新数据库相关upload-filter

---
+++ filter/upload/update-new-db.js
@@ -0,0 +1,60 @@
+"use strict";
+var
+async = require("async"),
+DB = require("../../util/db");
+
+
+function UpdateDB(options){
+    this.options = options;
+};
+
+
+UpdateDB.prototype = {
+    setup:function(){
+        this.env.updateList = [];
+    },
+    run: function(callback){
+
+        var table =  this.options.dbversion,
+            updatelist = this.env.updatelist,
+            db = new DB(),
+            tasks = [function(done){
+                db.connect(this.options.lion_newdb,function(err,conn,dbconfig){
+                    console.log("已连接数据库",dbconfig);
+                    done();
+                });
+            }];
+
+
+        updatelist.forEach(function(pair){
+            var key = pair.URL;
+            tasks.push(function(done){
+                var qs = db.sqlMaker("select",table,{},{URL:key});
+                
+                db.query(qs, function(err, rows) {
+                    if(err) throw err;
+                    var row = rows[0],
+                        query = row
+                            ? db.sqlMaker("update",table,pair,{
+                                URL:key
+                            })
+                            : db.sqlMaker("insert",table,pair);
+
+                    db.query(query,function(err){
+                        if(err) throw err;
+                        console.log((row?"更新":"插入") + " " + JSON.stringify(pair));
+                        done();
+                    });
+                });
+            });
+        });
+
+        async.series(tasks,function(err){
+            if(err){throw err;}else{
+                console.log("更新完成");
+                db.end();
+                done(null);
+            }
+        });
+    }
+};
