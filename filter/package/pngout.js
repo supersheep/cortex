3325535d1edaf08c60981845920b5abdadc6f68c
supersheep <supersheepxjy@gmail.com>
2012-12-20 18:24:55 +0800

    新增package filter pngout，压缩png

---
+++ filter/package/pngout.js
@@ -0,0 +1,16 @@
+var CompressBase = require("./compress-base");
+
+module.exports = {
+    create:function(config){
+        return new CompressBase({
+            config:config,
+            path:"pngout-"+process.platform,
+            command:"{dir} -y {path} {path}",
+            ext:["png"],
+            messages:{
+                "end":"png压缩处理完毕",
+                "compressed":"已压缩png文件{path}"
+            }
+        });
+    }
+};
